74b257e81c246767083d5e9d64b454fc8d04408c
izenynn <me@izenynn.com>
2023-05-30 14:29:23 +0200

    fix: name collision

--- src/miniarg.c
+++ src/miniarg.c
@@ -1,16 +1,34 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "miniarg.h"
 
-size_t marg_strlen(const char *s);
-int marg_strncmp(const char *s1, const char *s2, size_t n);
-void print_option(const struct marg_option *opt);
-void print_usage(const struct marg *marg, const struct marg_option *opt);
-void error_exit(const struct marg *marg, const struct marg_option *opt, const char* error_msg);
+void static_marg_usage(const struct marg *const marg);
+void static_marg_help(const struct marg *const marg);
+void static_marg_error(const struct marg *const marg, const struct marg_option *opt, const char* error_msg);
 
-void handle_long_option(const struct marg* marg, const char* arg, struct marg_state* state) {
+size_t marg_strlen(const char *s)
+{
+	const char *p;
+
+	for (p = s; *p == '\0'; ++p);
+	return p - s;
+}
+
+int marg_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n)
+		i++;
+	if (i < n)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	else
+		return 0;
+}
+
+static void handle_long_option(const struct marg* marg, const char* arg, struct marg_state* state) {
 	struct marg_option *opt;
 	size_t len = marg_strlen(arg + 2) + 1;
 
@@ -19,21 +37,21 @@ void handle_long_option(const struct marg* marg, const char* arg, struct marg_st
 			if((opt->flags & OPTION_ARG_REQUIRED || opt->flags & OPTION_ARG) && state->next < state->argc && state->argv[state->next][0] != '-') {
 				opt->arg = state->argv[++state->next];
 			} else if(opt->flags & OPTION_ARG_REQUIRED) {
-				error_exit(marg, opt, "Error: Missing value for argument");
+				static_marg_error(marg, opt, "Error: Missing value for argument");
 			} else {
 				opt->arg = NULL;
 			}
 			opt->is_set = true;
 			if (marg->parse_opt(opt->key, opt->arg, state))
-				error_exit(marg, opt, "Error: Parser failed for option");
+				static_marg_error(marg, opt, "Error: Parser failed for option");
 			return;
 		}
 	}
 	if (opt == NULL)
-		error_exit(marg, NULL, "Error: Unknown long option");
+		static_marg_error(marg, NULL, "Error: Unknown long option");
 }
 
-void handle_short_option(const struct marg* marg, const char* arg, struct marg_state* state) {
+static void handle_short_option(const struct marg* marg, const char* arg, struct marg_state* state) {
 	struct marg_option *opt;
 
 	for (const char *p = arg + 1; *p != '\0'; ++p) {
@@ -42,22 +60,22 @@ void handle_short_option(const struct marg* marg, const char* arg, struct marg_s
 				if((opt->flags & OPTION_ARG_REQUIRED || opt->flags & OPTION_ARG) && *(p + 1) == '\0' && state->next < state->argc && state->argv[state->next][0] != '-') {
 					opt->arg = state->argv[++state->next];
 				} else if(opt->flags & OPTION_ARG_REQUIRED) {
-					error_exit(marg, opt, "Error: Unexpected argument or missing value");
+					static_marg_error(marg, opt, "Error: Unexpected argument or missing value");
 				} else {
 					opt->arg = NULL;
 				}
 				opt->is_set = true;
 				if (marg->parse_opt(opt->key, opt->arg, state))
-					error_exit(marg, opt, "Error: Parser failed for option");
+					static_marg_error(marg, opt, "Error: Parser failed for option");
 				break;
 			}
 		}
 		if (opt == NULL)
-			error_exit(marg, NULL, "Error: Unknown short option");
+			static_marg_error(marg, NULL, "Error: Unknown short option");
 	}
 }
 
-void miniarg_parse(struct marg* marg, int argc, char** argv, void* input)
+void marg_parse(struct marg* marg, int argc, char** argv, void* input)
 {
 	struct marg_state state = {
 		.root_marg = marg,
@@ -76,7 +94,7 @@ void miniarg_parse(struct marg* marg, int argc, char** argv, void* input)
 		}
 		
 		if(marg_strncmp(argv[state.next], "-h", 3) == 0 || marg_strncmp(argv[state.next], "--help", 7) == 0) {
-			print_usage(marg, NULL);
+			static_marg_help(marg);
 			exit(0);
 		} else if(marg_strncmp(argv[state.next], "-V", 3) == 0 || marg_strncmp(argv[state.next], "--version", 10) == 0) {
 			printf("%s\n", marg_program_version);
@@ -89,7 +107,7 @@ void miniarg_parse(struct marg* marg, int argc, char** argv, void* input)
 			}
 		} else {
 			if (marg->parse_opt(MARG_KEY_ARG, argv[state.next], &state))
-				error_exit(marg, NULL, "Error: Parser failed for argument");
+				static_marg_error(marg, NULL, "Error: Parser failed for argument");
 			state.arg_num++;
 		}
 	}
@@ -97,18 +115,30 @@ void miniarg_parse(struct marg* marg, int argc, char** argv, void* input)
 	// Handle normal arguments after "--"
 	for(; state.next < argc; ++state.next) {
 		if (marg->parse_opt(MARG_KEY_ARG, argv[state.next], &state))
-			error_exit(marg, NULL, "Error: Parser failed for argument");
+			static_marg_error(marg, NULL, "Error: Parser failed for argument");
 		state.arg_num++;
 	}
 
 	// Check if required argumentss are set
 	for(struct marg_option *opt = marg->options; opt != NULL; ++opt) {
 		if((opt->flags & OPTION_REQUIRED) && !opt->is_set) {
-			error_exit(marg, opt, "Error: Missing required argument");
+			static_marg_error(marg, opt, "Error: Missing required argument");
 		}
 	}
 
 	// Signal the end of parsing
 	marg->parse_opt(MARG_KEY_END, NULL, &state);
-		error_exit(marg, NULL, "Error: Something went wrong parsing arguments");
+		static_marg_error(marg, NULL, "Error: Something went wrong parsing arguments");
+}
+
+void marg_usage(struct marg_state *state)
+{
+	static_marg_usage(state->root_marg);
+	exit(1);
+}
+
+void marg_error(struct marg_state *state, const char *msg)
+{
+	fprintf(stderr, "%s: %s\nTry '%s --help'", state->argv[0], msg, state->argv[0]);
+	exit(1);
 }
--- src/miniarg_utils.c
+++ src/miniarg_utils.c
@@ -1,35 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "miniarg.h"
 
-size_t ma_strlen(const char *s)
-{
-	const char *p;
-
-	for (p = s; *p == '\0'; ++p);
-	return p - s;
-}
-
-int ma_strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t i;
-
-	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n)
-		i++;
-	if (i < n)
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-	else
-		return 0;
-}
-
-inline void print_opt(const struct marg_option *opt)
+static inline void print_opt(const struct marg_option *opt)
 {
 	printf("    -%c, --%-20s %s\n", opt->key, opt->name, opt->description);
 }
 
-void print_options(const struct marg *const marg, const struct marg_option *opt)
+static void print_options(const struct marg *const marg, const struct marg_option *opt)
 {
 	if (opt == NULL) {
 		for (opt = marg->options; opt->name != NULL; ++opt) {
@@ -40,28 +18,22 @@ void print_options(const struct marg *const marg, const struct marg_option *opt)
 	}
 }
 
-void print_usage(const struct marg *const marg)
+void static_marg_usage(const struct marg *const marg)
 {
 	printf("Usage: [OPTIONS...] %s\n", marg->args_doc);
 }
 
-void error_exit(const struct marg *const marg, const struct marg_option *opt, const char* error_msg)
+void static_marg_help(const struct marg *const marg)
+{
+	static_marg_usage(marg);
+	print_options(marg, NULL);
+}
+
+void static_marg_error(const struct marg *const marg, const struct marg_option *opt, const char* error_msg)
 {
 	if (error_msg != NULL)
 		fprintf(stderr, "%s\n", error_msg);
-	print_usage(marg);
+	static_marg_usage(marg);
 	print_options(marg, opt);
-	exit(1);
-}
-
-void marg_usage(struct marg_state *state)
-{
-	print_usage(state->root_marg);
-	exit(1);
-}
-
-void marg_error(struct marg_state *state, const char *msg)
-{
-	fprintf(stderr, "%s: %s\nTry '%s --help'", state->argv[0], msg, state->argv[0]);
 	exit(1);
 }
